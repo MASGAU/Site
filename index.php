e9e5ab54bbbd849864b375ef523d506a6fabac0a
Matthew Barbour <sanmadjack@gmail.com>
2012-04-30 21:13:57 -0500

    Fix for dissapearing .js. Evil magic!

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 <link media="Screen" href="css/masgau.css" type="text/css" rel="stylesheet" />
 <link media="Screen" href="libs/jquery/css/dark-hive/jquery-ui-1.8.19.custom.css" type="text/css" rel="stylesheet" />
 <script type="text/javascript" src="libs/jquery/jquery-1.7.2.min.js"></script>
-<script type="text/javascript" src="libs/jquery/jquery-ui-1.8.19.custom.min"></script>
+<script type="text/javascript" src="libs/jquery/jquery-ui-1.8.19.custom.min.js"></script>
 <script type="text/javascript" src="libs/yoxview/yoxview-init.js"></script>
 <script type="text/javascript" src="javascript/masgau.js"></script>
 
